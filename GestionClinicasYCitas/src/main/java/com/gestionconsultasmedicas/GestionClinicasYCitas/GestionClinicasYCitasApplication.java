ca1bd78c87ecb9afc19a01758e8a4b46fb4a9612
wotzoy-2025592 <wotzoy-2025592@kinal.edu.gt>
2026-07-18 13:31:42 -0600

    feat: wotzoy-2025592 añadió la estructura inicial del proyecto junto a la configuración necesaria para la aplicación

---
+++ GestionClinicasYCitas/src/main/java/com/gestionconsultasmedicas/GestionClinicasYCitas/GestionClinicasYCitasApplication.java
@@ -0,0 +1,13 @@
+package com.gestionconsultasmedicas.GestionClinicasYCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClinicasYCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClinicasYCitasApplication.class, args);
+	}
+
+}
